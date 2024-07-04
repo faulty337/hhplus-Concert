ebdb1b10fdd1376f3999ec9d97d9cde6e177200a
DESKTOP-J12FN7D\fault <faulty337@gmail.com>
2024-07-04 23:51:06 +0900

    Feat : getSessionDate

--- concert/src/main/java/com/hhp/concert/MockAPIController.java
+++ concert/src/main/java/com/hhp/concert/MockAPIController.java
@@ -2,10 +2,15 @@ package com.hhp.concert;
 
 
 import com.hhp.concert.Presentation.dto.GetWaitingTokenResponseDto;
+import com.hhp.concert.Presentation.dto.GetSessionDateResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/mock/concert")
@@ -16,6 +21,14 @@ public class MockAPIController {
         return new GetWaitingTokenResponseDto(999, "token", false);
     }
 
+    @GetMapping("/session/{concertId}")
+    public List<GetSessionDateResponseDto> getSessionDate(@PathVariable String concertId){
+        List<GetSessionDateResponseDto> response = new ArrayList<>();
+        for(int i = 1; i < 24; i++){
+            response.add(new GetSessionDateResponseDto(i, LocalDate.now(), 23));
+        }
+        return response;
+    }
 
 
 }
