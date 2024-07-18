f2d81c89d961ef4bb1b57c2a10b9eb99bbc05be2
DESKTOP-A6GU9RJ\fault <faulty337@gmail.com>
2024-07-19 01:51:11 +0900

    Feat : JwtToken Filter

--- concert/src/main/java/com/hhp/concert/ConcertApplication.java
+++ concert/src/main/java/com/hhp/concert/ConcertApplication.java
@@ -2,8 +2,10 @@ package com.hhp.concert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ConcertApplication {
 
 	public static void main(String[] args) {
---
+++ concert/src/main/java/com/hhp/concert/util/filter/JwtWaitingAuthFilter.java
@@ -0,0 +1,63 @@
+package com.hhp.concert.util.filter;
+
+import com.hhp.concert.ApiControllerAdvice;
+import com.hhp.concert.util.JwtUtil;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class JwtWaitingAuthFilter implements Filter {
+
+    private static final Logger logger = LogManager.getLogger(JwtWaitingAuthFilter.class);
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("Initializing JwtWaitingAuthFilter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        logger.info("Request URI: {}", httpRequest.getRequestURI());
+
+        String header = httpRequest.getHeader(JwtUtil.AUTHORIZATION_HEADER);
+        String token = null;
+        String username = null;
+
+        logger.info("Authorization Header: {}", header);
+
+        if (header != null && header.startsWith("Bearer ")) {
+            token = header.substring(7);
+            username = jwtUtil.extractSign(token);
+
+            logger.info("Token: {}", token);
+            logger.info("Username: {}", username);
+        }
+
+        if (username != null && jwtUtil.validateToken(token)) {
+            logger.info("Token is valid, proceeding with the request.");
+            chain.doFilter(request, response);
+        } else {
+            logger.info("Token is invalid or not present, returning unauthorized status.");
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Destroying JwtWaitingAuthFilter");
+    }
+}
