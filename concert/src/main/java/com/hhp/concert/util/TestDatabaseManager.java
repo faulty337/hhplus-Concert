26b3a59982f247f358bf39b9a849491baef0bf67
faulty <faulty337@gmail.com>
2024-07-30 15:17:29 +0900

    Feat: 대용량 테스트 환경 구성

--- concert/src/main/java/com/hhp/concert/util/TestDatabaseManager.java
+++ concert/src/main/java/com/hhp/concert/util/TestDatabaseManager.java
@@ -1,23 +1,48 @@
 package com.hhp.concert.util;
 
+import com.hhp.concert.Business.Domain.Concert;
+import com.hhp.concert.Business.Domain.ConcertSeat;
+import com.hhp.concert.Business.Domain.ConcertSession;
+import com.hhp.concert.Business.Domain.User;
+import com.hhp.concert.Infrastructure.concert.ConcertJpaRepository;
+import com.hhp.concert.Infrastructure.seat.ConcertSeatJpaRepository;
+import com.hhp.concert.Infrastructure.session.ConcertSessionJpaRepository;
+import com.hhp.concert.Infrastructure.user.UserJpaRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Table;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Profile("test")
 @Component
+@RequiredArgsConstructor
 public class TestDatabaseManager implements InitializingBean {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+
+    private final ConcertJpaRepository concertJpaRepository;
+
+    private final ConcertSessionJpaRepository concertSessionJpaRepository;
+
+    private final ConcertSeatJpaRepository concertSeatJpaRepository;
+
+    private final UserJpaRepository userJpaRepository;
 
     private final List<String> tables = new ArrayList<>();
 
@@ -46,5 +71,47 @@ public class TestDatabaseManager implements InitializingBean {
             entityManager.createNativeQuery("ALTER TABLE " + table + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
         }
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+
+
+    @Transactional
+    public void init() {
+        Faker faker = new Faker();
+        List<Concert> concerts = new ArrayList<>();
+        List<ConcertSession> sessions = new ArrayList<>();
+        List<ConcertSeat> seats = new ArrayList<>();
+        List<User> users = new ArrayList<>();
+
+        for (int i = 0; i < 100000; i++) {
+            Concert concert = new Concert(faker.rockBand().name());
+            concerts.add(concert);
+            if (concerts.size() % 50 == 0) {
+                concertJpaRepository.saveAll(concerts);
+                concerts.clear();
+                entityManager.flush();
+                entityManager.clear();
+            }
+        }
+        if (!concerts.isEmpty()) {
+            concertJpaRepository.saveAll(concerts);
+            entityManager.flush();
+            entityManager.clear();
+        }
+
+        for (int i = 0; i < 1000000; i++) {
+            User user = new User(faker.name().fullName(), 10000);
+            users.add(user);
+            if (users.size() % 50 == 0) {
+                userJpaRepository.saveAll(users);
+                users.clear();
+                entityManager.flush();
+                entityManager.clear();
+            }
+        }
+        if (!users.isEmpty()) {
+            userJpaRepository.saveAll(users);
+            entityManager.flush();
+            entityManager.clear();
+        }
     }
 }
--- concert/src/test/java/com/hhp/concert/ConcertApplicationTests.java
+++ concert/src/test/java/com/hhp/concert/ConcertApplicationTests.java
@@ -1,13 +1,42 @@
 package com.hhp.concert;
 
+import com.hhp.concert.Business.Domain.Concert;
+import com.hhp.concert.Infrastructure.concert.ConcertJpaRepository;
+import com.hhp.concert.util.TestDatabaseManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class ConcertApplicationTests {
 
+	@Autowired
+	private TestDatabaseManager testDatabaseManager;
+	@Autowired
+	private ConcertJpaRepository concertJpaRepository;
+
+	@BeforeAll
+	public static void setUp(@Autowired TestDatabaseManager testDatabaseManager) {
+		testDatabaseManager.init();
+	}
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dataTest(){
+		List<Concert> concertList =concertJpaRepository.findAll();
+        assertEquals(100000, concertList.size());
 	}
 
 }
